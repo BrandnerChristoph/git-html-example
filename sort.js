a7cbededa5f7dc827ad1333f372f25db02251b6b
Brandner Christoph <bn@htlwy.at>
2023-10-03 14:03:26 +0200

    set to start js

--- sort.js
+++ sort.js
@@ -7,11 +7,7 @@ for(i = 0; i < 10000000; i++){
 // algorithmus zum sortieren bereitstellen
 const sort = () =>{
 	console.log('sort');
-	console.time('foreach-loop');
-	x.forEach((elem) => {
-		//
-	});
-	console.timeEnd('foreach-loop');
+	
 
 }
 
