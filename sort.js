433637c8b863e012721a08810bb7a2b11f654a17
Brandner Christoph <bn@htlwy.at>
2023-10-04 08:28:11 +0200

    sort done

--- sort.js
+++ sort.js
@@ -7,7 +7,13 @@ for(i = 0; i < 10000000; i++){
 // algorithmus zum sortieren bereitstellen
 const sort = () =>{
 	console.log('sort');
-	
+
+	console.time('for-loop');
+	for(idx = 0; idx < x.length; idx++){
+		///
+		
+	}
+	console.timeEnd('for-loop');	
 
 }
 
