5915cc38b784993708eca303242edc8b2323f26f
Brandner Christoph <bn@htlwy.at>
2023-10-05 07:14:59 +0200

    update recipe proc

--- recipe.js
+++ recipe.js
@@ -5,4 +5,7 @@ function loadRecipes() {
         return res.json(); 
     }) 
     .then((data) => console.log(data)); 
+
+    document.getElementById("suggestTxt").innerHTML ="";
+    document.getElementById("suggestTxt").innerHTML = "<p>Name: " + data[20].Name + "</p>";
 }
